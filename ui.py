8941c4b8aa55c864e22b96f6b2203c8b62d491f2
郭十二 <852568322@qq.com>
2024-10-19 23:50:30 +0800

    Update ui.py

--- ui.py
+++ ui.py
@@ -11,13 +11,25 @@ class WinGUI(Tk):
     def __init__(self):
         super().__init__()
         self.__win()
-        self.tk_button_Start = self.__tk_button_Start(self)
-        self.tk_input_input = self.__tk_input_input(self)
+        self.selected_value = IntVar()
+        self.selected_value.set(1)
+        self.tk_button_startButton = self.__tk_button_startButton(self)
+        self.tk_text_ipTxt = self.__tk_text_ipTxt(self)
+        self.tk_radio_button_dSelect = self.__tk_radio_button_dSelect(self)
+        self.tk_radio_button_pSelect = self.__tk_radio_button_pSelect(self)
+        self.tk_radio_button_selfSelect = self.__tk_radio_button_selfSelect(self)
+        self.tk_text_pathTxt = self.__tk_text_pathTxt(self)
+        self.tk_progressbar_progressBar = self.__tk_progressbar_progressBar(self)
+        self.tk_button_parseIpButton = self.__tk_button_parseIpButton(self)
+        self.tk_label_ipLabel = self.__tk_label_ipLabel(self)
+        self.tk_label_path = self.__tk_label_path(self)
+        self.tk_text_outputTxt = self.__tk_text_outputTxt(self)
+        self.tk_label_outputLabel = self.__tk_label_outputLabel(self)
     def __win(self):
-        self.title("Tool")
+        self.title("TkTool")
         # 设置窗口大小、居中
-        width = 600
-        height = 500
+        width = 500
+        height = 440
         screenwidth = self.winfo_screenwidth()
         screenheight = self.winfo_screenheight()
         geometry = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
@@ -58,14 +70,54 @@ class WinGUI(Tk):
             hbar = Scrollbar(master, orient="horizontal")
             self.h_scrollbar(hbar, widget, x, y, w, h, pw, ph)
         self.scrollbar_autohide(vbar, hbar, widget)
-    def __tk_button_Start(self,parent):
-        btn = Button(parent, text="Start", takefocus=False,)
-        btn.place(x=338, y=205, width=50, height=30)
+    def __tk_button_startButton(self,parent):
+        btn = Button(parent, text="开始", takefocus=False,)
+        btn.place(x=21, y=300, width=50, height=30)
         return btn
-    def __tk_input_input(self,parent):
-        ipt = Entry(parent, )
-        ipt.place(x=86, y=103, width=150, height=30)
-        return ipt
+    def __tk_text_ipTxt(self,parent):
+        text = Text(parent)
+        text.place(x=84, y=23, width=400, height=135)
+        return text
+    def __tk_radio_button_dSelect(self,parent):
+        rb = Radiobutton(parent,text="D", variable=self.selected_value, value=1)
+        rb.place(x=85, y=244, width=80, height=30)
+        return rb
+    def __tk_radio_button_pSelect(self,parent):
+        rb = Radiobutton(parent,text="P", variable=self.selected_value, value=2)
+        rb.place(x=183, y=244, width=80, height=30)
+        return rb
+    def __tk_radio_button_selfSelect(self,parent):
+        rb = Radiobutton(parent,text="自定义", variable=self.selected_value, value=3)
+        rb.place(x=282, y=244, width=80, height=30)
+        return rb
+    def __tk_text_pathTxt(self,parent):
+        text = Text(parent)
+        text.place(x=84, y=181, width=400, height=60)
+        return text
+    def __tk_progressbar_progressBar(self,parent):
+        progressbar = Progressbar(parent, orient=HORIZONTAL, maximum=100)
+        progressbar.place(x=84, y=299, width=400, height=30)
+        return progressbar
+    def __tk_button_parseIpButton(self,parent):
+        btn = Button(parent, text="解析IP", takefocus=False,)
+        btn.place(x=20, y=67, width=50, height=30)
+        return btn
+    def __tk_label_ipLabel(self,parent):
+        label = Label(parent,text="IP",anchor="center", )
+        label.place(x=20, y=24, width=50, height=30)
+        return label
+    def __tk_label_path(self,parent):
+        label = Label(parent,text="路径",anchor="center", )
+        label.place(x=20, y=182, width=50, height=30)
+        return label
+    def __tk_text_outputTxt(self,parent):
+        text = Text(parent)
+        text.place(x=84, y=340, width=400, height=80)
+        return text
+    def __tk_label_outputLabel(self,parent):
+        label = Label(parent,text="输出",anchor="center", )
+        label.place(x=20, y=342, width=50, height=30)
+        return label
 class Win(WinGUI):
     def __init__(self, controller):
         self.ctl = controller
@@ -74,7 +126,11 @@ class Win(WinGUI):
         self.__style_config()
         self.ctl.init(self)
     def __event_bind(self):
-        self.tk_button_Start.bind('<Button-1>',self.ctl.start)
+        self.tk_button_startButton.bind('<Button-1>',self.ctl.start)
+        self.tk_button_parseIpButton.bind('<Button-1>',self.ctl.parseIp)
+        self.tk_radio_button_dSelect.bind('<Button-1>',self.ctl.dSelete)
+        self.tk_radio_button_pSelect.bind('<Button-1>',self.ctl.pSelete)
+        self.tk_radio_button_selfSelect.bind('<Button-1>',self.ctl.selfSelete)
         pass
     def __style_config(self):
         pass
